b5628fdc694a1ef00155a6a1a34c1dee1a1b860a
Nick Wellman <wellman.nicholas@gmail.com>
2018-10-01 21:31:41 -0400

    switch to lombok

--- app/src/main/java/com/lordgasmic/recipe/repository/Property.java
+++ app/src/main/java/com/lordgasmic/recipe/repository/Property.java
@@ -2,41 +2,19 @@ package com.lordgasmic.recipe.repository;
 
 import com.lordgasmic.recipe.constants.DataType;
 
+import lombok.Data;
+
 /**
  * A property mapping between item descriptor and column
  *
  * Created by bruce on 3/2/17.
  */
-
+@Data
 class Property {
 
     private String name;
     private String columnName;
     private DataType dataType;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColumnName() {
-        return columnName;
-    }
-
-    public void setColumnName(String columnName) {
-        this.columnName = columnName;
-    }
-
-    public DataType getDataType() {
-        return dataType;
-    }
-
-    public void setDataType(DataType dataType) {
-        this.dataType = dataType;
-    }
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
